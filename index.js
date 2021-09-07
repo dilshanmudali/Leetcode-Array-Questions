5531394051a32b4751017ebc07f5c82cf3df68da
Dilshan Mudali <35578002+dilshanmudali@users.noreply.github.com>
2021-09-07 02:35:07 +0000

    rotated sorted arr solution

--- index.js
+++ index.js
@@ -182,10 +182,74 @@
 // maxSub([-2,1,-3,4,-1,2,1,-5,4]) //output - 6
 
 
-//--------------------- maximum product subarray
+//--------------------- maximum product subarray (((((REDOOO)))))
 
-function maxProdSub(arr){
-  
+// function maxProdSub(arr){
+//   let solution = arr[0];
+//   let currentMax = arr[0];
+//   let currentMin = arr[0];
+
+//   for(let i=1; i<arr.length; i++){
+    
+
+//     currentMax = Math.max(arr[i], currentMax[i-1]*arr[i], currentMin[i-1]*arr[i])
+//     currentMin = Math.min(arr[i], currentMax[i-1]*arr[i], currentMin[i-1]*arr[i])
+//     solution = Math.max(solution, currentMax[i])
+//   }
+//   return solution
+// }
+
+//  maxProdSub([-3,-2,-4]) 
+
+
+//-----------------------Find Minimum in Rotated Sorted Array
+
+// function minSortArr(nums){
+
+//   let left = 0;
+//   let right = nums.length - 1;
+
+//   while(left<right){
+//     let mid = Math.floor((left+right)/2);
+
+//     if(nums[mid] > nums[right]) left = mid + 1
+//     else right = mid
+//   }
+//  return nums[left]
+// }
+
+// minSortArr([3,4,5,1,2])
+
+
+//............................Search in rotated sorted Array
+
+nums = [4,5,6,7,8,0,1,2]
+target = 2
+//return index
+
+
+function rotatedSortArr(nums,target){
+
+  let left = 0
+  let right = nums.length-1
+
+  while(left<=right){
+    let mid = Math.floor((left+right)/2)
+
+    if(nums[mid] === target)return mid
+    
+    //determing which segment is sorted
+    if(nums[left]<=nums[mid]){
+      if(nums[left]<=target && target <= nums[mid]) right=mid-1
+      else left=mid+1
+    }else{
+      if(nums[mid]<=target && target<=nums[right]) left = mid+1
+      else right=mid-1
+    }
+
+  }
+  return -1
+
 }
 
- maxProdSub([-2,1,-3,4]) 
+rotatedSortArr(nums,target)
